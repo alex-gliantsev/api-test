d3b2a84586142915b9a7da660578f59e009a08f3
alex-gliantsev <sibitag@gmail.com>
2025-05-12 21:47:09 +0700

    Improve the configuration management

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-BASE_URL = "https://api.restful-api.dev/"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the BASE_URL from environment variables
+# Provide a default value if it's not set
+BASE_URL = os.getenv("BASE_URL", "https://api.restful-api.dev/") # Default fallback
